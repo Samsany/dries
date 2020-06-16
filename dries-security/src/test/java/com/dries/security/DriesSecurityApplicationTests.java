1fc35bb5f7a76c64146946c29f8b4d390a6adbee
samsany <1451691457@qq.com>
2020-06-16 19:23:06 +0800

    jwt

---
+++ dries-security/src/test/java/com/dries/security/DriesSecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dries.security;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DriesSecurityApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
